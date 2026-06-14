39b220ac53058af336eb54b70ebe130131d3ce52
Bosco <guobosco@163.com>
2026-06-14 23:03:32 +0800

    1

--- android_isanya/app/src/main/java/com/example/PeiPei/ui/screen/ExperienceCategoryFeedScreen.kt
+++ android_isanya/app/src/main/java/com/example/PeiPei/ui/screen/ExperienceCategoryFeedScreen.kt
@@ -4,6 +4,7 @@ package com.example.Lulu.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
